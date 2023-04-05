4ccefd5007301d10d965e6094ea108f10cc61c33
bilalrajput09 <ranabilalrajput09@gmail.com>
2023-04-05 08:43:30 +0200

    Add files and configed webpack

---
+++ src/getClasses.js
@@ -0,0 +1,10 @@
+import { wizard } from "./wizard"
+import { barbarian } from "./barbarian.js";
+import { get } from "lodash-es";
+
+function getClasses() {
+    console.log(wizard);
+    console.log(barbarian);
+}
+
+export default getClasses;
